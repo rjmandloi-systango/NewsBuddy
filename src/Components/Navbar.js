6afa18598b36c29fcce85534ed7d9954761e26f7
rjmandloi-systango <rjmandloi@isystango.com>
2022-05-31 15:17:28 +0530

    Dynamic news implimentation and paging

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-
 export class Navbar extends Component {
     static propTypes = {
 
@@ -9,7 +7,7 @@ export class Navbar extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
                         <a className="navbar-brand" href="/">NewsBuddy</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,7 +19,7 @@ export class Navbar extends Component {
                                     <a className="nav-link active" aria-current="page" href="/">Home</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">About</a>
+                                    <a className="nav-link active" href="/about">About</a>
                                 </li>
                             </ul>
                         </div>
--- src/NewsItem.js
+++ src/NewsItem.js
@@ -13,7 +13,7 @@ export class NewsItem extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{discription}</p>
-            <a href={newsUrl} target="_blank" className="btn btn-primary btn-sm">Read more</a>
+            <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-dark btn-sm">Read more</a>
           </div>
         </div>
       </div>
